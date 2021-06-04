085b84b56c4826b3d8ba88e582ea2d0525edf76a
Mega Farts <clint.m.thompson@outlook.com>
2021-06-04 15:01:04 -0500

    Merge

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -4,11 +4,10 @@ from django.db import models
 
 
 class Employees(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=50)
-    user = models.ForeignKeyField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self
