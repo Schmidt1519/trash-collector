6a62b5364d9eb055334ce451968a1f57e38a762b
unknown <schmidt1519@gmail.com>
2021-06-04 15:00:23 -0500

    Updated customers and employees model attributes. Created a create method on customers views for user to build a profile. Created create.html and updated customers urls to run the create method. Added link to customers index page to create profile.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -2,22 +2,16 @@ from django.db import models
 # Create your models here.
 
 
-class Customers(models.Model):
-    id = models.IntegerField(primary_key=True)
+class Customer(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=50)
+    pickup_day = models.CharField(max_length=50)
     balance = models.IntegerField()
     one_time_pickup = models.DateField()
     suspension_start = models.DateField()
     suspension_end = models.DateField()
-    user = models.ForeignKeyField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self
-# TODO: Finish customer model by adding necessary properties to fulfill user stories
-
-
-class Customer(models.Model):
-    name = models.CharField(max_length=50)
-    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,15 +1,12 @@
 from django.db import models
-
 # Create your models here.
 
 
 class Employees(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=50)
-    user = models.ForeignKeyField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self
-# TODO: Create an Employee model with properties required by the user stories
