99b79522048caa79aafc58e523fb162871792b35
Mega Farts <clint.m.thompson@outlook.com>
2021-06-04 12:38:23 -0500

    Created model classes for customers and employees

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
+class Employees(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    user = models.ForeignKeyField(max_length=50)
+
+    def __str__(self):
+        return self
 # TODO: Create an Employee model with properties required by the user stories
